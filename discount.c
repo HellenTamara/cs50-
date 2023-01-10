ea4f4723b199bc93badc91ea95d893782f7412bf
HellenTamara <hellen.tamara@outlook.com>
2023-01-10 20:19:23 +0900

    10/01/23

---
+++ discount.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+float discount(float price, int percentage);
+
+int main (void)
+{
+    float regular = get_float("Regular price: ");
+    int percent_off = get_int("Percent off: ");
+    float sale = discount(regular, percent_off);
+    printf("Sale price: %.2f\n", sale);
+}
+
+float discount(float price, int percentage)
+{
+    return price * (100 - percentage) / 100;
+}
